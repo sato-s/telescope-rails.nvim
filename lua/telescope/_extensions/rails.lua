2ad65fed9bff83955c86864baab5a18fbdf76291
sato-s <s.sato.desu@gmail.com>
2022-06-12 02:24:19 +0900

    changed display style

--- lua/telescope/_extensions/rails.lua
+++ lua/telescope/_extensions/rails.lua
@@ -5,7 +5,23 @@ local conf = require("telescope.config").values
 local entry_display = require("telescope.pickers.entry_display")
 local action_state = require("telescope.actions.state")
 
-local flatten = vim.tbl_flatten
+local path_to_file_name = function(path)
+  local last;
+  for match in (path.."/"):gmatch("(.-)".."/") do
+     last = match
+  end
+  return last
+end
+
+local path_to_class_name = function(path)
+  s = path_to_file_name(path)
+  return s:sub(1, -4)
+end
+
+local displayer = function(table)
+  path = table["value"]
+  return path_to_class_name(path)
+end
 
 local find_rails = function(target, opts)
   opts = opts or {}
@@ -14,25 +30,19 @@ local find_rails = function(target, opts)
     prompt_title = target,
     finder = finders.new_oneshot_job({
       "find", path, "-type", "f",
+    },
+    {
       entry_maker = function(entry)
+        print(type(entry))
         return {
-          value = "hoge",
-          -- display = entry[1],
-          display = "hoge",
-          ordinal = entry[1],
+          value = entry,
+          display = displayer,
+          ordinal = entry,
         }
       end
-    }),
+    }
+    ),
     sorter = conf.generic_sorter(opts),
-    -- attach_mappings = function(prompt_bufnr, map)
-    --   actions.select_default:replace(function()
-    --     actions.close(prompt_bufnr)
-    --     local selection = action_state.get_selected_entry()
-    --     -- print(vim.inspect(selection))
-    --     vim.api.nvim_put({ selection[1] }, "", false, true)
-    --   end)
-    --   return true
-    -- end,
   }):find()
 end
 
