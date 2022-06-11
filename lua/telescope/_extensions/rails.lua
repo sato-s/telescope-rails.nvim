a17558fd27f006c86fd7e36ecc3d466e47ae7426
sato-s <s.sato.desu@gmail.com>
2022-06-11 21:12:23 +0900

    find . command

--- lua/telescope/_extensions/rails.lua
+++ lua/telescope/_extensions/rails.lua
@@ -10,32 +10,31 @@ local colors = function(opts)
   opts = opts or {}
   pickers.new(opts, {
     prompt_title = "colors",
-    finder = finders.new_table {
-      results = {
-        { "red", "#ff0000" },
-        { "green", "#00ff00" },
-        { "blue", "#0000ff" },
-      },
+    finder = finders.new_oneshot_job({
+      "find", "-type", "f",
       entry_maker = function(entry)
+        print(entry)
         return {
           value = entry,
-          display = entry[1],
+          -- display = entry[1],
+          display = "hoge",
           ordinal = entry[1],
         }
       end
-    },
+    }),
     sorter = conf.generic_sorter(opts),
-    attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local selection = action_state.get_selected_entry()
-        -- print(vim.inspect(selection))
-        vim.api.nvim_put({ selection[1] }, "", false, true)
-      end)
-      return true
-    end,
+    -- attach_mappings = function(prompt_bufnr, map)
+    --   actions.select_default:replace(function()
+    --     actions.close(prompt_bufnr)
+    --     local selection = action_state.get_selected_entry()
+    --     -- print(vim.inspect(selection))
+    --     vim.api.nvim_put({ selection[1] }, "", false, true)
+    --   end)
+    --   return true
+    -- end,
   }):find()
 end
+colors()
 
 return require("telescope").register_extension({
   exports = {
